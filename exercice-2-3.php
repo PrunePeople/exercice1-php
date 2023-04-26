e9ca86b94735a6db662ea090d04bf41619843521
Audrey <audrey.dedieu@beecoming.fr>
2023-04-26 16:03:11 +0200

    modif titre

--- exercice-2-3.php
+++ exercice-2-3.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercice 2 - Capitales</title>
+    <title>Exercice 2 - Départements</title>
 </head>
 
 <body>
